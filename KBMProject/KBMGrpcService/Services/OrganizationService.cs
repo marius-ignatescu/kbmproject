1fc828445746a6b205bd0d8ecee7097086bc620b
Marius Ignatescu <marius@descopera.org>
2025-06-30 15:14:32 +0300

    Defined organization proto

---
+++ KBMProject/KBMGrpcService/Services/OrganizationService.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using KBMGrpcService.Protos;
+
+namespace KBMGrpcService.Services
+{
+    public class OrganizationService : OrganizationProtoService.OrganizationProtoServiceBase
+    {
+        public override async Task<CreateOrganizationResponse> CreateOrganization(CreateOrganizationRequest request, ServerCallContext context)
+        {
+            return new CreateOrganizationResponse();
+        }
+
+        public override async Task<OrganizationResponse> GetOrganizationById(GetOrganizationByIdRequest request, ServerCallContext context)
+        {
+            return new OrganizationResponse();
+        }
+
+        public override async Task<QueryOrganizationsResponse> QueryOrganizations(QueryOrganizationsRequest request, ServerCallContext context)
+        {
+            return new QueryOrganizationsResponse();
+        }
+
+        public override async Task<OrganizationResponse> UpdateOrganization(UpdateOrganizationRequest request, ServerCallContext context)
+        {
+            return new OrganizationResponse();
+        }
+
+        public override async Task<DeleteOrganizationResponse> DeleteOrganization(DeleteOrganizationRequest request, ServerCallContext context)
+        {
+            return new DeleteOrganizationResponse();
+        }
+    }
+}
--- KBMProject/KBMGrpcService/Services/UserService.cs
+++ KBMProject/KBMGrpcService/Services/UserService.cs
@@ -1,5 +1,5 @@
 using Grpc.Core;
-using KBMGrpcService;
+using KBMGrpcService.Protos;
 
 namespace KBMGrpcService.Services
 {
@@ -11,8 +11,29 @@ namespace KBMGrpcService.Services
             _logger = logger;
         }
 
-        //public override async Task<CreateUserResponse> CreateUser(CreateUserRequest request, ServerCallContext context)
-        //{
-        //}
+        public override async Task<CreateUserResponse> CreateUser(CreateUserRequest request, ServerCallContext context)
+        {
+            return new CreateUserResponse();
+        }
+
+        public override async Task<UserResponse> GetUserById(GetByIdRequest request, ServerCallContext context)
+        {
+            return new UserResponse();
+        }
+
+        public override async Task<QueryUsersResponse> QueryUsers(QueryUsersRequest request, ServerCallContext context)
+        {
+            return new QueryUsersResponse();
+        }
+
+        public override async Task<UserResponse> UpdateUser(UpdateUserRequest request, ServerCallContext context)
+        {
+            return new UserResponse();
+        }
+
+        public override async Task<DeleteResponse> DeleteUser(DeleteUserRequest request, ServerCallContext context)
+        {
+            return new DeleteResponse();
+        }
     }
 }
