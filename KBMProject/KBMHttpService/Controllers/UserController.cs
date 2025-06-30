9902acc549d308d88c64eb5027da186a66c046e6
Marius Ignatescu <marius@descopera.org>
2025-06-30 15:32:43 +0300

    Added user controller and proto files

---
+++ KBMProject/KBMHttpService/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using KBMGrpcService.Protos;
+using KBMGrpcService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KBMHttpService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserProtoService.UserProtoServiceClient _client;
+
+        public UserController(UserProtoService.UserProtoServiceClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var response = await _client.GetUserByIdAsync(new GetByIdRequest { Id = id });
+            return Ok(response);
+        }
+    }
+}
