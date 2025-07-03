324765e2fa2dbd49046a80e139b9bdb88e107187
Marius Ignatescu <marius@descopera.org>
2025-07-03 11:23:12 +0300

    Added connection string in appsettings

--- KBMProject/KBMGrpcService/Program.cs
+++ KBMProject/KBMGrpcService/Program.cs
@@ -1,4 +1,3 @@
-using KBMGrpcService;
 using KBMGrpcService.Data;
 using KBMGrpcService.Services;
 using Microsoft.EntityFrameworkCore;
@@ -10,17 +9,19 @@ builder.Services.AddGrpc();
 builder.WebHost.UseUrls("http://0.0.0.0:5001");
 
 // Database Context
-var dbHost = Environment.GetEnvironmentVariable("DB_HOST"); //"172.24.192.1"
-var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-var connectionString = $"Server={dbHost}; Database={dbName}; User ID=sa;Password={dbPassword}; TrustServerCertificate=True;";
+//var dbHost = Environment.GetEnvironmentVariable("DB_HOST");// ?? "172.24.192.1";
+//var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+//var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+//var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+//var connectionString = $"Server={dbHost}; Database={dbName}; User ID={dbUser};Password={dbPassword}; TrustServerCertificate=True;";
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
-Console.WriteLine($"[DEBUG] DB_HOST = {dbHost}");
-Console.WriteLine($"[DEBUG] DB_NAME = {dbName}");
-Console.WriteLine($"[DEBUG] DB_SA_PASSWORD = {dbPassword}");
+//Console.WriteLine($"[DEBUG] DB_HOST = {dbHost}");
+//Console.WriteLine($"[DEBUG] DB_NAME = {dbName}");
+//Console.WriteLine($"[DEBUG] DB_SA_PASSWORD = {dbPassword}");
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<UserService>();
